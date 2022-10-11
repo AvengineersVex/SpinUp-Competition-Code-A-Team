074dc6674a9b95e5053b9faa3214edb7289d021c
jindo85547 <imjustin679@gmail.com>
2022-10-11 16:51:05 -0700

    Initial drive train code
    10/11/22

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,53 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    1, 3            
+// Controller1          controller                    
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
 
 using namespace vex;
 
+extern controller Controller1;
+extern motor RightDriveSmart;
+extern motor LeftDriveSmart;
+
+competition Competition = competition();
+
+void usercontrol(void)
+{
+  RightDriveSmart.setVelocity(0.0, velocityUnits::pct);
+  LeftDriveSmart.setVelocity(0.0, velocityUnits::pct);
+
+  RightDriveSmart.spin(directionType::fwd);
+  LeftDriveSmart.spin(directionType::rev);
+
+  while(true)
+  {
+    if(abs(Controller1.Axis3.position()) > 20)
+    {
+      RightDriveSmart.setVelocity(Controller1.Axis3.position() + 27, velocityUnits::pct);
+      LeftDriveSmart.setVelocity(Controller1.Axis3.position() + 27, velocityUnits::pct);
+    }
+
+    if(abs(Controller1.Axis1.position()) > 0) 
+    {
+      RightDriveSmart.setVelocity(Controller1.Axis1.position(), velocityUnits::pct);
+      LeftDriveSmart.setVelocity(Controller1.Axis1.position(), velocityUnits::pct);
+    }
+
+    wait(20, msec);
+  }
+
+}
+
+
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
   
+  Competition.drivercontrol(usercontrol);
 }
