cf2351b21572c39acb68d7f50c399795ab29ca4e
Sathvik Parasa <64510758+sathvikparasa@users.noreply.github.com>
2022-10-25 16:56:10 -0700

    Attempt one at intake
    Vansh Chhabra went through pain.

--- include/robot-config.h
+++ include/robot-config.h
@@ -5,6 +5,7 @@ extern brain Brain;
 // VEXcode devices
 extern drivetrain Drivetrain;
 extern controller Controller1;
+extern motor Intake;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Text.
